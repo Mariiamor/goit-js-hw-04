bbcfdeb5a72ff27bb8379943132cc3f676e9cbb2
Mariia Khanelia <mariiapiskorska@gmail,com>
2025-01-28 20:16:06 +0100

    add js file

--- js/task-2.js
+++ js/task-2.js
@@ -3,9 +3,11 @@ function calcAverageCalories(days) {
     return 0;
   }
 
-  const totalCalories = days.reduce(function (total, day) {
-    return total + day.calories;
-  }, 0);
+  let totalCalories = 0;
+  
+  for (let i = 0; i < days.length; i++) {
+    totalCalories += days[i].calories;
+  }
 
   return totalCalories / days.length;
 }
